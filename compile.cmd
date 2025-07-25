802c44cfc0bf61c53d53292ae2b3c90f73dda8fa
kasenra <sapphismgang@gmail.com>
2025-07-25 17:34:31 -0500

    Bonus 1 and 2. Little Sister Robot Incest will take over the world.

--- compile.cmd
+++ compile.cmd
@@ -1,2 +1,2 @@
-tweego -f sugarcube-2 -o index.html -s 30Days src/core src/secret src/style src/scripts src/testing -l
+tweego -f sugarcube-2 -o index.html -s 30Days src/core src/secret src/style src/scripts src/testing src/bonus -l
 index.html
